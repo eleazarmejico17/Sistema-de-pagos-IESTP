1cb6888e70023810c8aa2b5c5ce280d74edf1578
Yadhi Huamani <167116887+Yadhi12Miku8@users.noreply.github.com>
2025-11-26 10:20:05 -0500

    .

--- views/dashboard-admin.php
+++ views/dashboard-admin.php
@@ -75,7 +75,7 @@ function activo($id, $pagina){
           <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
           <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
         </span>
-        <p class="text-sm text-gray-300">...</p>
+        <p class="text-sm text-gray-300">Administrador</p>
       </div>
     </div>
 
